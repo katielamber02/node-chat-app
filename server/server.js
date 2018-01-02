1b0fd064658d9fa14ca575d8ea026ea785e5992b
saikham <saikham.email@gmail.com>
2018-01-02 13:50:34 +0630

    Add welcome message and alert others

--- server/server.js
+++ server/server.js
@@ -12,13 +12,31 @@ var io = socketIO(server);
 io.on('connection', (socket) =>  {
 	console.log('new user connected');
 
+	//socket.emit from admin text welcome
+	socket.emit('newMessage',{
+		from: 'Admin',
+		text: 'Welcome from chat app',
+		createdAt: new Date().getTime()
+	});
+	//socket.broadcast.emit from admin text new user joined
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createdAt: new Date().getTime()
+	});
+
 	socket.on('createMessage', (message) => {
 		console.log('createMessage :', message);
 		io.emit('newMessage',{
 			from: message.from,
-			texxt: message.text,
+			text: message.text,
 			createdAt: new Date().getTime()
 		});
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// });
 	})
 
 	socket.on('disconnect', () => {
